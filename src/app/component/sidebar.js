22c0e9c8ca6eee57e9ad17e35b692edf2b946909
Veggie711 <arnavsingh011205@gmail.com>
2025-06-24 14:39:44 +0530

    Good

---
+++ src/app/component/sidebar.js
@@ -0,0 +1,31 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function RootLayout({ children }) {
+
+  const navItems = [
+    { label: 'Home', href: '/' },
+    { label: 'Join Room', href: '/join' },
+    { label: 'Analyze Chats', href: '/analyze' },
+    { label: 'My Account', href: '/account' },
+  ];
+
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/5 bg-[#1e2021] p-4 flex flex-col justify-center items-center gap-4">
+        <div className='flex flex-col gap-4 align-center'>
+            {navItems.map(({ label, href }) => (
+                <Link
+                key={label}
+                href={href}
+                className="min-w-60 rounded-md hover:bg-zinc-700 transition text-3xl px-6 py-4 block text-white"
+                >
+                {label}
+                </Link>
+            ))}
+            </div>
+        </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-import Home from "./home/page.js"
+import Home from "./Home/page.js"
+import Sidebar from "./component/sidebar";
+
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
         <Home></Home>
+        <Sidebar></Sidebar>
       </body>
     </html>
   );
