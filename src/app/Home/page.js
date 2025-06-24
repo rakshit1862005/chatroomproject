62067f7e1f82984aff315c1dd76cd72d6a491a4e
rakshit1862005 <95012114+rakshit1862005@users.noreply.github.com>
2025-06-24 14:46:07 +0530

    roomcard

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -2,11 +2,6 @@ import Homecard from "../roomcardhome"
 
 export default function Home(){
     return(
-<<<<<<< Updated upstream
         <div></div>
-=======
-        <div><Homecard></Homecard></div>
-
->>>>>>> Stashed changes
     )
 }
