e1386b14c86c185c6ed78f320594e0c838c11e2f
rakshit1862005 <95012114+rakshit1862005@users.noreply.github.com>
2025-06-24 17:40:27 +0530

    tailwind update

--- src/app/component/sidebar.js
+++ src/app/component/sidebar.js
@@ -2,8 +2,7 @@
 
 import Link from 'next/link';
 
-export default function Sidebar({ }) {
-
+export default function Sidebar() {
   const navItems = [
     { label: 'Home', href: '/' },
     { label: 'Join Room', href: '/join' },
@@ -13,19 +12,19 @@ export default function Sidebar({ }) {
 
   return (
     <div className="flex h-screen">
-      <div className="w-1/5 bg-[#1e2021] p-4 flex flex-col justify-center items-center gap-4">
-        <div className='flex flex-col gap-4 align-center'>
-            {navItems.map(({ label, href }) => (
-                <Link
-                key={label}
-                href={href}
-                className="min-w-60 rounded-md hover:bg-zinc-700 transition text-3xl px-6 py-4 block text-white"
-                >
-                {label}
-                </Link>
-            ))}
-            </div>
+      <div className="w-1/5 bg-[#252424] p-4 flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col gap-4 align-center">
+          {navItems.map(({ label, href }) => (
+            <Link
+              key={label}
+              href={href}
+              className="min-w-65 min-h-15 rounded-md hover:bg-zinc-700 transition text-2xl font-light px-6 py-4 text-white flex items-center"
+            id='bartext'>
+              {label}
+            </Link>
+          ))}
         </div>
+      </div>
     </div>
   );
 }
