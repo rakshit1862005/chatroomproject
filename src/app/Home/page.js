ea2095ee549ae117aed13e7719b5903db485862e
rakshit1862005 <95012114+rakshit1862005@users.noreply.github.com>
2025-07-10 16:03:02 +0530

    Add Socket.IO chat functionality to Home page
    Integrated socket.io and socket.io-client for real-time messaging. Added a message input and send button to the Home page, with logic to emit and receive chat messages via Socket.IO. Updated styles for the message input field.

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -3,9 +3,23 @@ import Homecard from "../roomcardhome"
 import Sidebar from "../component/sidebar"
 import Login from "../myaccount/page"
 import { useEffect, useState } from "react"
+import io from 'socket.io-client'
+
+const socket = io.connect('http://localhost:8000');
+
+
+async function send(message) {
+    socket.emit("send_message",{message:message});
+}
 
 export default function Home(){
     const [userid,setuserid] = useState(null)
+    useEffect(()=>{
+        socket.on("recieve_message",(data)=>{
+            console.log(data);
+        })
+    },[socket])
+    
     useEffect(()=>{
         const temp = localStorage.getItem('userid');
         if(temp){
@@ -18,11 +32,14 @@ export default function Home(){
             <div className="mainarea">
             {userid?(
                 <div>
+                
                 <div className="wcom">Welcome {userid}!</div>
                 <div id="dot">
                 <div id="chatdisplay">
                     <img src='/images/add.svg' id="addbox"></img>
                     <h2 id="jnr">---- Join Rooms Now ----</h2>
+                    <input id="message" type="text"></input>
+                    <button id="logout" onClick={()=>{send(document.getElementById('message').value)}}>send</button>
                 </div>
                 </div>
                 </div>
